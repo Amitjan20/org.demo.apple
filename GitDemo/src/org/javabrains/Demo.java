8d12b6aa3915f25bb0995aee8de4841de894532a
amitjan20 <aslekaramit@gmail.com>
2017-03-12 20:53:28 -0700

    hhh

--- GitDemo/src/org/javabrains/Demo.java
+++ GitDemo/src/org/javabrains/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 		System.out.println("hello git");
 		System.out.println("one step commit");
 System.out.println("trying merging again");
+System.out.println("merging back to master");
 		// TODO Auto-generated method stub
 
 
